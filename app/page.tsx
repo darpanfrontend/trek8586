c623ad9cc1130a60dfaf2448c11b441ff7598218
darpanfrontend <officiallydarpan@gmail.com>
2024-06-03 22:59:25 +0545

    seconds

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,16 @@ import TopScrollAnim from './components/js/TopScrollAnim';
 export default async function Home(): Promise<JSX.Element> {
   return (
     <>
+    <div className='fixed w-full top-0 left-0 z-[100] m-8'>
+      <div className='relative h-[70px] w-full text-center'>
+        <Image
+          src="/logo.png"
+          fill={true}
+          alt="Picture of the author"
+          className='w-full object-contain'
+          />
+      </div>
+    </div>
       <TopScrollAnim />
     </>
   )
